66aee4618545f2c3ed3cc4fca5bcf70d50891f80
claycat <catenarywkd@naver.com>
2024-03-05 22:47:30 +0900

    add mui + emotion

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import Button from '@mui/material/Button';
 
 const LoginPage = () => {
     return (
         <div>
             <h1>Home</h1>
             <p>가장 먼저 보여지는 페이지입니다.</p>
+            <Button variant="contained">BUTTON</Button>
         </div>
     );
 };
