a16e8a260d9f3ae1fd98e1a4f7704db1e208045a
dla0510 <dla0510@naver.com>
2024-03-07 22:53:09 +0900

    publish payment confirm page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from 'pages/login/LoginPage';
 import MyPage from 'pages/mypage/MyPage';
+import PaymentConfirmPage from 'pages/payment/PaymentConfirmPage';
 
 function App() {
     return (
         <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/mypage" element={<MyPage />} />
+            <Route path="/payment-complete" element={<PaymentConfirmPage />} />
         </Routes>
     );
 }
