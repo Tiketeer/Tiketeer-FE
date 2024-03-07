cdfd9ca275f99b02eb98e4944aa9545d16098d75
One-armed-boy <fanatic5500@gmail.com>
2024-03-07 15:13:12 +0900

    MyPage 내 SideBar 추가

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,7 +1,32 @@
+import styled from '@emotion/styled';
 import PageHeader from 'components/page-header/PageHeader';
+import MyPageSideBar from './Sidebar';
+
+const MyPageContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`;
+
+const bodyPadding = 5;
+const MyPageBodyContainer = styled.div`
+    width: ${100 - 2 * bodyPadding}%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    padding: 0% ${bodyPadding}%;
+`;
 
 const MyPage = () => {
-    return <PageHeader isLogined={true} />;
+    return (
+        <MyPageContainer>
+            <PageHeader isLogined={true} />
+            <MyPageBodyContainer>
+                <MyPageSideBar />
+            </MyPageBodyContainer>
+        </MyPageContainer>
+    );
 };
 
 export default MyPage;
