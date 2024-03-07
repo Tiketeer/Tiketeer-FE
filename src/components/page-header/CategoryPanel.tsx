da6b0f3c3db452db8e6638964d0076153282013e
One-armed-boy <fanatic5500@gmail.com>
2024-03-07 11:19:10 +0900

    CategoryPanel 내 마우스오버 효과 추가

--- src/components/page-header/CategoryPanel.tsx
+++ src/components/page-header/CategoryPanel.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { COLOR } from 'color/color';
 
 export const CategoryList = ['Concerts', 'Arts', 'Conference', 'Movies', 'International'];
 
@@ -12,17 +13,31 @@ export const CategoryContainer = styled.div`
 
 export const CategoryElementContainer = styled.div`
     padding: 10px;
+`;
+
+export const CategoryElementBtn = styled.button`
     font-size: 18px;
     font-weight: bold;
     text-align: center;
     font-family: Abhaya Libre;
+    background: white;
+    border: none;
+    transition: 0.5s;
+    &:hover {
+        color: ${COLOR.DEFAULT};
+        transition: 0.5s;
+    }
 `;
 
 const CategoryPanel = () => {
     return (
         <CategoryContainer>
             {CategoryList.map(category => {
-                return <CategoryElementContainer>{category}</CategoryElementContainer>;
+                return (
+                    <CategoryElementContainer>
+                        <CategoryElementBtn>{category}</CategoryElementBtn>
+                    </CategoryElementContainer>
+                );
             })}
         </CategoryContainer>
     );
